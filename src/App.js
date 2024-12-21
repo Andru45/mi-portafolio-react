011931c90d06ef311970d2364f750bf8361d4516
Andru45 <andrujoseruiz@gmail.com>
2024-12-21 14:03:47 -0400

    Actualización: Animaciones AOS y soporte para tema oscuro

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Proyectos from "./Componentes/Proyectos";
 import SobreMi from "./Componentes/SobreMi";
 import Contacto from "./Componentes/Contacto";
 import Habilidades from "./Componentes/Habilidades";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Importa los estilos de AOS
+import AOS from "aos";
+import "aos/dist/aos.css"; // Importa los estilos de AOS
 
 function App() {
   useEffect(() => {
     AOS.init({
       duration: 1000, // Duración de las animaciones en milisegundos
-      easing: 'ease-in-out', // Tipo de animación
+      easing: "ease-in-out", // Tipo de animación
       once: true, // Solo anima una vez por elemento
     });
   }, []);
@@ -31,10 +31,18 @@ function App() {
         {darkMode ? "☀️ Tema Claro" : "🌙 Tema Oscuro"}
       </button>
 
-      <SobreMi />
-      <Habilidades />
-      <Proyectos />
-      <Contacto />
+      <div data-aos="fade-up">
+        <SobreMi />
+      </div>
+      <div data-aos="fade-right">
+        <Habilidades />
+      </div>
+      <div data-aos="zoom-in">
+        <Proyectos />
+      </div>
+      <div data-aos="flip-left">
+        <Contacto />
+      </div>
 
       <footer>© 2024 Andru - Todos los derechos reservados.</footer>
     </div>
